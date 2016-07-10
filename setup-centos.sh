7344b527a67b926bdf348b3705b602a2bdc6888e
Tokuhiro Matsuno <tokuhirom@gmail.com>
2016-07-10 13:46:15 +0900

    install golang from binary tar ball

--- setup-centos.sh
+++ setup-centos.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 sudo yum install -y epel-release
 sudo yum install -y tmux screen
-sudo yum install -y git vim python-pip zsh rubygem-bundler golang
+sudo yum install -y git vim python-pip zsh rubygem-bundler
 sudo yum install -y openssl-devel ruby-devel
 sudo yum install -y java-1.8.0-openjdk-devel java-1.8.0-headless
 sudo yum groupinstall -y 'Development tools'
