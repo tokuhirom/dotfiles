c8ec460b1f4738e44d5dc7a9b8c640c0604491f3
Tokuhiro Matsuno <tokuhirom@gmail.com>
2019-02-24 15:10:08 +0000

    w3m is required

--- setup-debian.sh
+++ setup-debian.sh
@@ -4,7 +4,7 @@ curl -sL https://deb.nodesource.com/setup_11.x | sudo bash -
 sudo apt-get update
 sudo apt-get upgrade
 
-sudo apt-get -y install nodejs
+sudo apt-get -y install nodejs w3m
 sudo apt-get -y install build-essential
 sudo apt-get -y install cpanminus
 sudo apt-get -y install python3 iputils-ping tmux vim-nox
