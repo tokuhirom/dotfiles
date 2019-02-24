2f7920db59569d5b402aa6f612271ced0fc31f19
Tokuhiro Matsuno <tokuhirom@gmail.com>
2019-02-24 00:05:03 +0000

    tmux is requierd

--- setup-debian.sh
+++ setup-debian.sh
@@ -3,7 +3,7 @@ curl -sL https://deb.nodesource.com/setup_11.x | sudo bash -
 sudo apt-get -y install nodejs
 sudo apt-get -y install build-essential
 sudo apt-get -y install cpanminus
-sudo apt-get -y install python3 iputils-ping linuxbrew-wrapper
+sudo apt-get -y install python3 iputils-ping linuxbrew-wrapper tmux
 # note. i can't install android-studio on pixelbook using linuxbrew.
 sudo apt autoremove
 
