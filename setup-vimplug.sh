74a26cbc7f764bf593272b98fbb17275f235917c
Tokuhiro Matsuno <tokuhirom@gmail.com>
2020-03-10 11:44:33 +0900

    change vim-plug url

--- setup-vimplug.sh
+++ setup-vimplug.sh
@@ -2,6 +2,7 @@
 
 # install vimplug
 if [ ! -f ~/.vim/autoload/plug.vim ]; then
-    curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+	curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
+	    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 fi
 vim +PlugInstall +qall
