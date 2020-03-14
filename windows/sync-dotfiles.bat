8f89b5f7e0418c8772c4a3cf980992d2c0ba9aaa
Tokuhiro Matsuno <tokuhirom@gmail.com>
2020-03-14 16:48:30 +0900

    'sync'

--- windows/sync-dotfiles.bat
+++ windows/sync-dotfiles.bat
@@ -1,10 +1,6 @@
-#!/bin/bash
-set -ex
-
-cd $HOME/dotfiles
+cd C:\Users\tokuh\dotfiles
 git add .
 git commit -a -m 'sync'
 
 git push origin master
 git pull origin master
-
--- windows/sync.bat
+++
@@ -1,2 +0,0 @@
-cmd /c ""C:\Program Files\Git\git-bash.exe" --login -i -- C:\Users\tokuh\dotfiles\sync.sh"
-pause
