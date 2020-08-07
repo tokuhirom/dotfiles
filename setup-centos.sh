3dbe085f9a7d38447f797a94e2e949e231d8e6a0
Tokuhiro Matsuno <tokuhirom@gmail.com>
2020-08-07 15:36:49 +0900

    install tree

--- setup-centos.sh
+++ setup-centos.sh
@@ -4,6 +4,6 @@ sudo yum install -y epel-release
 sudo yum install -y tmux screen
 sudo yum install -y git vim python-pip zsh rubygem-bundler
 sudo yum install -y openssl-devel ruby-devel
-sudo yum install -y java-11-openjdk
+sudo yum install -y java-11-openjdk tree
 sudo yum groupinstall -y 'Development tools'
 
--- setup-ubuntu.sh
+++ setup-ubuntu.sh
@@ -4,8 +4,8 @@ sudo apt-get update
 sudo apt-get upgrade
 
 sudo apt -y install w3m curl
+sudo apt -y install nodejs tree
 sudo apt -y install build-essential
-sudo apt -y install cpanminus perl-doc
 sudo apt -y install openjdk-11-jdk-headless
 sudo apt -y install python3 iputils-ping tmux vim-nox silversearcher-ag
 sudo apt -y install zsh
