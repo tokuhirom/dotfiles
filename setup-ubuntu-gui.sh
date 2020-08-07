5812f57cc797464038929729845ab55a08c8de35
Tokuhiro Matsuno <tokuhirom@gmail.com>
2020-08-07 11:00:01 +0900

    sync

--- setup-ubuntu-gui.sh
+++ setup-ubuntu-gui.sh
@@ -7,7 +7,7 @@ sudo apt install -y fonts-ipafont fonts-noto
 sudo apt install -y fonts-noto-cjk fonts-noto-cjk-extra
 sudo apt install -y language-pack-ja
 
-# 便利ツール。
+# xfce4 便利ツール。
 sudo apt install -y xfce4-goodies
 
 # shohex san said, fcitx is better than ibus.
@@ -21,8 +21,6 @@ sudo snap install inkdrop
 sudo snap connect inkdrop:password-manager-service
 
 
-# sudo apt install -y ibus-mozc
-
 # for dayflower's LINE vpn script
 # PulseSecure/NSA
 sudo apt install -y openconnect python3-metaconfig
@@ -32,21 +30,30 @@ sudo apt install -y python2 python-keyring
 sudo apt install xfce4
 sudo apt install lightdm
 
+# -------------------------------------------------------------------------
+# purge too heavy DM
+# -------------------------------------------------------------------------
 sudo apt purge gdm3
 
+# -------------------------------------------------------------------------
 # albert
 # Alfred like command launcher.
 # https://albertlauncher.github.io/docs/installing/
+# -------------------------------------------------------------------------
 curl https://build.opensuse.org/projects/home:manuelschneid3r/public_key | sudo apt-key add -
 echo 'deb http://download.opensuse.org/repositories/home:/manuelschneid3r/xUbuntu_20.04/ /' | sudo tee /etc/apt/sources.list.d/home:manuelschneid3r.list
 sudo wget -nv https://download.opensuse.org/repositories/home:manuelschneid3r/xUbuntu_20.04/Release.key -O "/etc/apt/trusted.gpg.d/home:manuelschneid3r.asc"
 sudo apt update
 sudo apt install albert
 
+# -------------------------------------------------------------------------
+# libreoffice
+# -------------------------------------------------------------------------
+sudo apt install -y libreoffice-calc
 
-# --------------------------------------------------------------
+# -------------------------------------------------------------------------
 # wine for LINE app
-# --------------------------------------------------------------
+# -------------------------------------------------------------------------
 sudo apt install wine winbind winetricks
 # install IME
 winetricks allfonts
