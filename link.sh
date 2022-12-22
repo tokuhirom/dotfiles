7df687685757119a121109fd0bcd4f08824948a3
Tokuhiro Matsuno <tokuhirom@gmail.com>
2022-12-22 09:54:41 +0900

    i3

---
+++ link.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+set -e
+
+function link {
+    local fname=$1
+    local src="$HOME/dotfiles/$fname"
+    local dst="$HOME/$fname"
+    if [ ! -e $dst ]; then
+        mkdir -p $(dirname $dst)
+        echo "Linking $src $dst"
+        if [ -e /git-bash.exe ]; then
+            # windows' git-bash doesn't support symlink
+            ln "$src" "$dst"
+        else
+            ln -s "$src" "$dst"
+        fi
+    else
+        echo "Exists $dst"
+    fi
+}
+
+link .config/i3/config
+
+mkdir -p ~/.vim/tmp/
+
