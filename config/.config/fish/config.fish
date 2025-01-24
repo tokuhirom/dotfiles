6c4ef90db31f1e821f077ee95a1d17ea1a2d5ef9
Tokuhiro Matsuno <tokuhirom@gmail.com>
2025-01-24 11:47:04 +0900

    setup some paths for fish

--- config/.config/fish/config.fish
+++ config/.config/fish/config.fish
@@ -2,6 +2,10 @@ if status is-interactive
     # Commands to run in interactive sessions can go here
     set fish_greeting
 
+    if test -d /opt/homebrew
+        fish_add_path --global --move --path "/opt/homebrew/bin" "/opt/homebrew/sbin"
+    end
+
     # https://ka2n.hatenablog.com/entry/2017/01/09/fish_shell%E3%81%A7z%E3%81%AE%E7%B5%90%E6%9E%9C%E3%82%92peco%E3%81%A3%E3%81%A6%E7%88%86%E9%80%9F%E3%81%A7%E3%83%87%E3%82%A3%E3%83%AC%E3%82%AF%E3%83%88%E3%83%AA%E7%A7%BB%E5%8B%95%E3%81%99%E3%82%8B
     function fzf_z
       # すでに入力済みの文字列があればそれを fzf のクエリとして入力済みにする
@@ -40,5 +44,16 @@ if status is-interactive
     abbr -a s ls
     abbr -a l ls
     abbr -a ll ls -l
+
+    if type -q nvim
+        abbr -a vim nvim
+    end
+
+    if test -d ~/.plenv
+        fish_add_path ~/.plenv/bin
+        eval "$(plenv init -)"
+    end
+
+    fish_add_path ~/dotfiles/bin/
 end
 
