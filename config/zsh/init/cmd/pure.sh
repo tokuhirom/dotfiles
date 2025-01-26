09e3d06ebbdce9d62e7b807db332ced7e5e481a7
Tokuhiro Matsuno <tokuhirom@gmail.com>
2025-01-27 05:24:20 +0900

    better configuration for pure

--- config/zsh/init/cmd/pure.sh
+++ config/zsh/init/cmd/pure.sh
@@ -1,10 +1,12 @@
 if [ -d /opt/homebrew ]; then
     fpath+=("/opt/homebrew/share/zsh/site-functions")
-
-    autoload -U promptinit; promptinit
-    prompt pure
 else
-    echo "pure not available"
+    if [ ! -d "$HOME/.zsh/pure" ]; then
+        mkdir -p "$HOME/.zsh"
+        git clone https://github.com/sindresorhus/pure.git "$HOME/.zsh/pure"
+    fi
+    fpath+=($HOME/.zsh/pure)
 fi
 
-
+autoload -U promptinit; promptinit
+prompt pure
