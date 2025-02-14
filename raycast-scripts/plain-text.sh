541b1c016c453d22c6411545fc837b359e906c2f
Tokuhiro Matsuno <tokuhirom@gmail.com>
2025-02-14 14:36:26 +0900

    plain text

---
+++ raycast-scripts/plain-text.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# Required parameters:
+# @raycast.schemaVersion 1
+# @raycast.title plain text
+# @raycast.mode silent
+
+# Optional parameters:
+# @raycast.icon 🤖
+
+# Documentation:
+# @raycast.description make plain text
+# @raycast.author tokuhirom
+# @raycast.authorURL https://raycast.com/tokuhirom
+
+pbpaste | pbcopy
