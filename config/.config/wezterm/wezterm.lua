b9c7842cbb8c25dfb58d257084a0c995bfdcda98
Tokuhiro Matsuno <tokuhirom@gmail.com>
2026-01-09 16:47:27 +0900

    tab のフォントサイズ大きくする

--- config/.config/wezterm/wezterm.lua
+++ config/.config/wezterm/wezterm.lua
@@ -9,6 +9,10 @@ return {
     -- hide_tab_bar_if_only_one_tab = true,
     -- adjust_window_size_when_changing_font_size = false,
 
+    window_frame = {
+        font_size = 16.0,
+    },
+
     keys = {
         {
             key = 'C',
