a2d424837db487845957fac5f49118bb71fc2aa6
Tokuhiro Matsuno <tokuhirom@gmail.com>
2026-01-14 09:50:19 +0900

    cheat command's completion

---
+++ config/zsh/init/cmd/cheat.sh
@@ -0,0 +1,17 @@
+# cheat command completion
+_cheat() {
+    local cheat_dir="$HOME/dotfiles/cheat"
+    local -a cheat_files
+
+    # Get list of cheat sheets (without .md extension)
+    cheat_files=(${cheat_dir}/*.md(:t:r))
+
+    _arguments \
+        '-e[edit cheat sheet]' \
+        '-l[list cheat sheets]' \
+        '--edit[edit cheat sheet]' \
+        '--list[list cheat sheets]' \
+        '1:cheat sheet:($cheat_files)'
+}
+
+compdef _cheat cheat
