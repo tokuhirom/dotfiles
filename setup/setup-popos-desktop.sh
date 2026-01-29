6d0faaa630f00608925573c658417f1ae093ce9d
Tokuhiro Matsuno <tokuhirom@gmail.com>
2026-01-29 23:30:51 +0900

    install some libraries for akaza

--- setup/setup-popos-desktop.sh
+++ setup/setup-popos-desktop.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
-sudo apt install -y neovim zsh git-lfs docker
+sudo apt install -y neovim zsh git-lfs docker vim silversearcher-ag
 sudo apt install -y tmux ripgrep bvi jq tree w3m curl
 sudo apt install -y valgrind cmake nodejs
 
 # desktop
 sudo apt install -y xsel gnome-tweaks slack
 
+# akaza deps
+sudo apt install libgdk-pixbuf-2.0-dev libgtk-4-dev libibus-1.0-dev libmarisa-dev
