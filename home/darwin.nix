a72cc8baa596303a4e8444b73f5ecb9b5241ca58
Tokuhiro Matsuno <tokuhirom@gmail.com>
2026-02-03 15:53:07 +0900

    home/darwin.nix から Brewfile と重複するパッケージを削除

--- home/darwin.nix
+++ home/darwin.nix
@@ -1,52 +1,16 @@
 { pkgs, lib, ... }: lib.mkIf pkgs.stdenv.isDarwin {
   # macOS-specific home-manager configuration
+  # ADR-0015: Homebrew を優先。Brewfile にないものだけここで管理
 
   home.packages = with pkgs; [
-    # common.nix から移行
-    vim
-    tmux
-    fzf
-    ripgrep
-    bat
-    jq
-    fd
-    gh
-    cmake
+    # Brewfile にないパッケージ
     gnumake
-    automake
-    mercurial
     python3
-    python3Packages.ipython
     ruby
     luajit
-    luajitPackages.luarocks
-    coreutils
-    silver-searcher
-    yq-go
-    pandoc
-    bottom
-    btop
-    entr
-    curl
-    wget
-    nmap
-    whois
-    docker-client
-    # kind
-    # k9s
-    awscli2
-    go-task
-    just
-    sops
-    mkcert
-    duckdb
-    graphviz
-    ditaa
     direnv
     nix-direnv
     devbox
-    mise
-    zellij
     tree
     htop
     watch
