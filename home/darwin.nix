487cf559aa9d8af4905157a4703d1980b40978fa
Tokuhiro Matsuno <tokuhirom@gmail.com>
2026-02-03 15:55:49 +0900

    nix-direnv, devbox を削除

--- home/darwin.nix
+++ home/darwin.nix
@@ -1,11 +1,11 @@
 { pkgs, lib, ... }: lib.mkIf pkgs.stdenv.isDarwin {
   # macOS-specific home-manager configuration
-  # ADR-0015: Homebrew を優先。Nix 専用パッケージのみここで管理
+  # ADR-0015: Homebrew を優先
+  #
+  # 現在、すべてのパッケージは Homebrew (Brewfile) で管理されています
 
   home.packages = with pkgs; [
-    # Nix 専用パッケージ
-    nix-direnv
-    devbox
+    # Homebrew にないパッケージがあればここに追加
   ];
 
 }
