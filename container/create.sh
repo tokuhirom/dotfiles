9109a37c320cb6a635b5ea4cd43419e3adc063d5
Tokuhiro Matsuno <tokuhirom@gmail.com>
2026-06-13 08:09:04 +0900

    container 関連不要
    Signed-off-by: Tokuhiro Matsuno <tokuhirom@gmail.com>

--- container/create.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-container machine create alpine:3.24.0 --name dev
-container machine set-default dev
-
--- container/setup.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-# nkf はなかった
-container machine run -n dev --root apk add vim zsh curl bash git libstdc++ libgcc uv w3m jq tree wget the_silver_searcher ripgrep
-
